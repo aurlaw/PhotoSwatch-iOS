81205ee867aa488139ec1e5cc87056eaf2b2195c
Michael Lawrence <mlawrence@aurlaw.com>
2013-08-18 19:14:35 -0400

    Initial Commit

---
+++ PhotoSwatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoSwatch
+//
+//  Created by Michael Lawrence on 8/18/13.
+//  Copyright (c) 2013 Michael Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PhotoSwatch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoSwatch
+//
+//  Created by Michael Lawrence on 8/18/13.
+//  Copyright (c) 2013 Michael Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
